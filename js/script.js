68c1483a29d4d9c5f792baf61290af92df955d95
Michael <m.molin.1987@gmail.com>
2020-02-19 11:15:42 +0100

    pulizia

--- js/script.js
+++ js/script.js
@@ -5,36 +5,24 @@
 //  applicare sconto
 //  restituire prezzo finale
 
-
-
 var etaUtente = prompt ("Digitare l'età dell'utente: ");
 var numKm = prompt ("Quanti Km desidera viaggiare utilizzando i nostri treni? ");
-var sconto;
-var scontoTesto = false;
-var prezzoFinale;
-
-
-console.log(etaUtente);
 
 if (etaUtente < 18) {
-    sconto = 20;
-    scontoTesto = true;
+    var sconto = 20;
+    var scontoTesto = true;
 } else if (etaUtente >= 65 ) {
-    sconto = 40;
-    scontoTesto = true;
-} else {
-    sconto = 0;
-}
-
-
+    var sconto = 40;
+    var scontoTesto = true;
+} else {}
 
 if (scontoTesto) {
-    prezzoFinale = ((0.21 * numKm) - (((0.21 * numKm) * sconto) /100));
+    var prezzoFinale = ((0.21 * numKm) - (((0.21 * numKm) * sconto) /100));
     prezzoFinale= prezzoFinale.toFixed(2);
     document.getElementById('Testo').setAttribute('class' , 'visible');
     document.getElementById('Testo').innerHTML = "In base alla sua età anagrafica avrà uno sconto pari al: " + sconto + "%" + ", quindi il suo prezzo finale sarà di " + prezzoFinale + "€ per " + (numKm) + "km";
 } else {
-    prezzoFinale = (0.21 * numKm);
+    var prezzoFinale = (0.21 * numKm);
     prezzoFinale= prezzoFinale.toFixed(2);
     document.getElementById('Testo').setAttribute('class' , 'visible');
     document.getElementById('Testo').innerHTML = "In base alla sua età anagrafica non ha diritto ad uno sconto, quindi il suo prezzo finale sarà di: " + prezzoFinale + "€ per " + (numKm) + "km";
